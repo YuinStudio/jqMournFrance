10c5743560df988358ca1dc6ec15e5284a4838e4
nobita <unvisityboss@gmail.com>
2015-11-17 14:29:21 +0800

    add effect leaveTime

--- src/js/jqmournfrance.js
+++ src/js/jqmournfrance.js
@@ -1,10 +1,29 @@
-"use strict";
 (function ($) {
-    $.fn.jqmournfrance = function () {
+    'use strict';
+    var defaultOpt = {
+        effect: 'hide',
+        duration: 0,
+        leaveTime: 3000
+    }, idList = {};
+
+    $.fn.jqmournfrance = function (options) {
+        if (typeof options !== 'object') {
+            options = {};
+        }
+
+        options = $.extend(defaultOpt, options);
+
         $.each(this, function () {
             var $this = $(this);
             $this.addClass('mourn-france-main-box').append(jQuery.mfTemplate.mournFrance);
-        });
+            setTimeout(function () {
+                $this.find('> .mourn-france')[options.effect](options.duration, function () {
+                    $(this).remove();
+                });
+                $this.removeClass('mourn-france-main-box');
+            }, options.leaveTime);
 
+
+        });
     };
 })(jQuery);
